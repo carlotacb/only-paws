564a1ad8bc393fcafe747afe61f995e91718e6ca
carlotacb <carkbra@gmail.com>
2022-09-17 02:29:23 -0400

    first configure

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const config = require('./config')
+// const BootstrapRouter = require('./routes')
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+// BootstrapRouter(app)
+BootstrapServer(app)
+StartServer(app)
+
+function BootstrapServer(app) {
+    app.use(function (err, req, res, next) { // eslint-disable-line
+        console.error(`Error on ${req.method} ${req.path} with request body ${JSON.stringify(req.body)}\n`, err)
+        return res.status(err.status || 500).json({errorCode: err.errorCode, message: err.message})
+    })
+}
+
+function StartServer(app) {
+    app.listen(config.port, function () {
+        console.log(`Backend listening on port ${config.port}!`)
+    })
+}
+
