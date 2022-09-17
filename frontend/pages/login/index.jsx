2e4092d72081436d00741695c0df1ec0e57e147b
Tanya <tan@v1042-wn-rt-a-36-27.campus-dynamic.uwaterloo.ca>
2022-09-17 16:04:24 -0400

    some frontend

--- frontend/pages/login/index.jsx
+++ frontend/pages/login/index.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Logo from '../../assets/Logo.png';
+import Image from 'next/image';
+import { Button } from 'primereact/button';
+import { InputTextarea } from 'primereact/inputtextarea';
+
 
 const Login = () => {
   return (
-    <div> Login</div>
+    <div className='login-container'>
+      <Image src={Logo} alt="logo" height={100} width={180}/>
+      <div  className='login-box' style={{justifyItems: 'center',alignItems:'center'}}>
+        <InputTextarea className ="login-input" rows={2} cols={40} value="Type in your Username" onChange=""/>
+        <InputTextarea className ="login-input" rows={2} cols={40} value="Type in your Username" onChange=""/>
+        <Button class="login-btn" label="Submit"  onClick=""/>
+      </div>
+      </div>
   )
 }
 
--- frontend/pages/signup/index.js
+++ frontend/pages/signup/index.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Card } from 'primereact/card';
+import Logo from '../../assets/Logo.png';
+import Image from 'next/image';
+import { Button } from 'primereact/button';
 
+import { InputTextarea } from 'primereact/inputtextarea';
+ // sorry misnamed this one this is actually login
+
+ 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='signup-container'>
+      <Image src={Logo} alt="logo" height={100} width={180}/>
+      <div  className='signup-box' style={{justifyItems: 'center',alignItems:'center'}}>
+        <InputTextarea className ="signup-input" rows={2} cols={40} value="Type in your Username" onChange=""/>
+        <InputTextarea className ="signup-input" rows={2} cols={40} value="Type in your Username" onChange=""/>
+        <Button class="signup-btn" label="Submit" rows={20} onClick=""/>
+      </div>
+      
+    </div>
   )
 }
 
