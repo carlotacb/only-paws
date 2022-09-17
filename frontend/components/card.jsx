9959170b09ed1977a73e3016c53e84e16ccd423f
Tanya <tan@v1042-wn-rt-a-36-27.campus-dynamic.uwaterloo.ca>
2022-09-17 19:11:15 -0400

    third frontend commit

--- frontend/components/card.jsx
+++ frontend/components/card.jsx
@@ -4,16 +4,16 @@ import Image from 'next/image';
 import 'primeicons/primeicons.css';
 
 
-
-const Card = () => {
+// this is the profile
+const Profile = () => {
   return (
-    <div className='card-container'>
+    <div className='profile-container'>
         <Image src={sample} alt="logo" />
-        <div className='btn-container-card'>
-            <button classname='btn-card' style={{backgroundColor: '#F8333C'}}>
+        <div className='btn-container-profile'>
+            <button classname='btn-profile' style={{backgroundColor: '#F8333C'}}>
                 <i className='pi pi-check' style={{color : "white" , backgroundColor: '#F8333C'}}></i>
             </button>
-            <button classname='btn-card' style={{backgroundColor: '#F8333C'}}>
+            <button classname='btn-profile' style={{backgroundColor: '#F8333C'}}>
                 <i className='pi pi-times' style={{color : "white" , backgroundColor: '#44AF69'}}></i>
             </button>
         </div>
@@ -22,4 +22,4 @@ const Card = () => {
   )
 }
 
-export default Card;;
+export default Profile;
