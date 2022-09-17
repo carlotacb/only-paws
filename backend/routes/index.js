f347451cee5df350f2c13c22683da8f7d05beeef
Edward Boado <eboado8862@conestogac.on.ca>
2022-09-17 14:55:16 -0400

    Add stubs for put, post apis

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,4 +10,17 @@ module.exports = app => {
         return res.send({status: 'up'})
     })
 
+    // Create new user profile
+    app.post('/user/create', (req, res) => {
+        // Save to the db
+
+        return res.status(200).send('Profile created')
+    })
+
+    // Updated existing user profile
+    app.put('/user/:id/update', (req, res) => {
+        // Update the db
+
+        return res.status(200).send('Profile updated')
+    })
 }
