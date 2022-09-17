4a789fedd7f74850eb8bfa7779c93d1f37743fbe
Edward Boado <eboado8862@conestogac.on.ca>
2022-09-17 19:03:34 -0400

    Updated login page styling

--- frontend/pages/login/index.jsx
+++ frontend/pages/login/index.jsx
@@ -12,11 +12,11 @@ const Login = () => {
   }
   return (
     <form className='login-container'>
-      <Image src={Logo} alt="logo" height={100} width={180}/>
+      <Image src={Logo} alt="logo" height={150} width={230}/>
       <div  className='login-box' style={{justifyItems: 'center',alignItems:'center'}}>
-        <InputTextarea className ="login-input" rows={2} cols={40} value="Type in your Email" onChange="" required/>
-        <InputTextarea className ="login-input" rows={2} cols={40} value="Password goes here" onChange="" required/>
-        <Button class="login-btn" label="Submit"  onClick={handleSubmit}/>
+        <InputTextarea className ="login-input" rows={1.5} cols={40} value="Type in your Email" onChange="" required/>
+        <InputTextarea className ="login-input" rows={1.5} cols={40} value="Password goes here" onChange="" required/>
+        <Button class="login-btn" label="Submit" height={200} width={350} onClick={handleSubmit}/>
    
       </div>
       </form>
