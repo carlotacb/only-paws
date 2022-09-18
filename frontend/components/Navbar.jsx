55aef0b3c74183de1808aa6e1266a2848395075c
Tanya <tan@v1042-wn-rt-a-36-27.campus-dynamic.uwaterloo.ca>
2022-09-18 04:20:28 -0400

    just the linking functions left

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -4,6 +4,13 @@ import 'primeicons/primeicons.css';
 import Logo from '../assets/Logo.png';
 import Image from 'next/image';
 
+
+//add link to the personal profile of the user by extracting the user from the back-end
+// add a function for logging out 
+
+const Logout = () =>{
+
+}
 const Navbar = () => {
   return (
     <div className="navbar-container">
@@ -11,10 +18,12 @@ const Navbar = () => {
             <Link href="/"><Image src={Logo} alt="logo" height={100} width={120}/></Link>
         </p>
         <div className="btn-container">
-            <button type="button" className="profile-icon" onClick="">
-                <i className='pi pi-user'></i>
-            </button>
-            <button type="button" className="logout-icon" onClick="">
+            <Link href="/">
+                <button type="button" className="profile-icon" onClick="">
+                    <i className='pi pi-user'></i>
+                </button>
+            </Link>
+            <button type="button" className="logout-icon" onClick={Logout}>
                 <i className='pi pi-sign-out'></i>
             </button>
         </div>
--- frontend/pages/login/index.jsx
+++ frontend/pages/login/index.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Logo from '../../assets/Logo.png';
 import Image from 'next/image';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/InputText';
+import Link from 'next/link';
 
 
 const Login = () => {
 
-  const handleSubmit =() =>{
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+
+  // to be done by backend
+  const handleSubmit =() =>{
+    
   }
   return (
     <form className='login-container'>
       <Image src={Logo} alt="logo" height={150} width={230}/>
       <div  className='login-box'>
-        <InputText className ="login-input" placeholder="Type in your Email" onChange="" required/>
-        <InputText className ="login-input" placeholder="Password goes here" onChange="" required/>
+        <InputText className ="login-input" placeholder="Type in your Email" onChange={(e)=> setEmail(e.target.value)} required/>
+        <InputText className ="login-input" placeholder="Password goes here" onChange={(e)=> setEmail(e.target.value)} required/>
         <Button class="login-btn" label="Submit" height={150} width={250} onClick={handleSubmit}/>
+        <p>New User? Sign up <Link style={{color: "blue"}}href="/signup">here</Link></p>
    
       </div>
       </form>
