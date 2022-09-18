a07ffd845fc3232d4182af7726ffaaefa9c55c41
Tanya <tan@v1042-wn-rt-a-36-27.campus-dynamic.uwaterloo.ca>
2022-09-18 02:21:48 -0400

    everything except the message window is done

--- frontend/components/card.jsx
+++ frontend/components/card.jsx
@@ -4,22 +4,22 @@ import Image from 'next/image';
 import 'primeicons/primeicons.css';
 
 
-// this is the profile
-const Profile = () => {
+const Card = () => {
   return (
-    <div className='card-container'>
-        <Image src={sample} alt="logo" />
-        <div className='btn-container-card'>
-            <button classname='btn-card' style={{backgroundColor: '#F8333C'}}>
-                <i className='pi pi-check' style={{color : "white" , backgroundColor: '#F8333C'}}></i>
-            </button>
-            <button classname='btn-card' style={{backgroundColor: '#44AF69'}}>
-                <i className='pi pi-times' style={{color : "white" , backgroundColor: '#44AF69'}}></i>
-            </button>
+    <div className='card-window'>
+        <button className='btn-card' style={{backgroundColor: '#F8333C'}}>
+                <i className='pi pi-times' style={{color : "white" , backgroundColor: '#F8333C'}}></i>
+        </button>
+        <div className='card-container'>
+            <Image className="card-img" src={sample} alt="logo" />
         </div>
-        
+        <button className='btn-card' style={{backgroundColor: '#44AF69'}}>
+                <i className='pi pi-check' style={{color : "white" , backgroundColor: '#44AF69'}}></i>
+        </button>
     </div>
+    
+
   )
 }
 
-export default Profile;
+export default Card;
